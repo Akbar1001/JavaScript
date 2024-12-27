f160b43851f43891ad80e43e8b1f3f9473ff8dcf
Akbar1001 <149526516+Akbar1001@users.noreply.github.com>
2024-12-27 15:08:25 +0000

    updated

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+//let is prefered over var keyword 
+//the default value for any uninitialized variable is Undefined
+let accountid=122334
+let accEmail="hello@gmail.com";
+let acc_city="Kanpur"
+let accSal;
+console.table([accountid,accEmail,acc_city,accSal])
