d510e17eaf2d4070629971d244a4cc9c017d94b1
Akbar1001 <149526516+Akbar1001@users.noreply.github.com>
2024-12-28 18:25:28 +0000

    updated

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -3,5 +3,5 @@
 let accountid=122334
 let accEmail="hello@gmail.com";
 let acc_city="Kanpur"
-let accSal;
-console.table([accountid,accEmail,acc_city,accSal])
+let accSalary;
+console.table([accountid,accEmail,acc_city,accSalary])
