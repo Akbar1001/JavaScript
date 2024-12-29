43022bb3b8907f3adac5ae60d47df9f18a1d9618
Akbar1001 <149526516+Akbar1001@users.noreply.github.com>
2024-12-29 15:57:49 +0000

    done

---
+++ 01_basics/01_operations.js
@@ -0,0 +1,10 @@
+let a,b,c;
+a=b=c=6*5  //all variable get assigned the same value
+console.log(a);
+console.log(b);
+console.log(c);
+//tricky Operations in Js
+console.log("1"+2+4);   // o/p -> 124
+console.log(1+2+"4");   // o/p -> 34 (consider the left most element data type)
+
+// === does strict check that is compare value as well as data type of two variables/consonants
