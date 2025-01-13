e6131b01d80d7b70071efd1ec958d6943cd3d2f9
Akbar1001 <149526516+Akbar1001@users.noreply.github.com>
2025-01-13 18:08:39 +0000

    updated

--- 02_basics/04_Objects.js
+++ 02_basics/04_Objects.js
@@ -22,7 +22,7 @@ const RegularUser={
 //1st method (less used)
 const obj1={1:"a",2:"b"}
 const obj2={2:"c",3:"d"}
-const obj3= Object.assign({},obj1,obj2);    // key '2' whose value is changed to last '2' key value has 
+const obj3= Object.assign(obj1,obj2);    // key '2' whose value is changed to last '2' key value has 
 console.log(obj3); // O/p --> { '1': 'a', '2': 'c', '3': 'd' }
  
 // 2nd way( Most Used) = Speard technique
