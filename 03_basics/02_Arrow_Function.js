2e39638c063d8a4b87099f41b448e5e073f9e1c9
Akbar1001 <149526516+Akbar1001@users.noreply.github.com>
2025-02-01 17:01:38 +0000

    updated

--- 03_basics/02_Arrow_Function.js
+++ 03_basics/02_Arrow_Function.js
@@ -15,3 +15,20 @@ user.welcomeMessage() //calling function welcomeMessage
 //NOte
 // value of "this" keyword in NODE js enviroment is {}(an Empty object).
 // value of "this" keyword in console of browser enviroment is Window{} <- window object.
+
+//Syntax of Arrow function
+let hello = ()=>{
+    console.log("what the time");
+}
+hello();
+
+// Remember
+let sum= (n1,n2)=>{
+    return n1+n2;
+}
+console.log(sum(2,8));  //10
+// BUT the above function is same as below function
+
+let add = (n1,n2)=> (n1+n2)
+console.log(add(8,9));  // 
+// what message it is conveying ifs that when we use parenthesis we use return keyword but when we use small brackets we DONOT use return keyword as it will explicitly return the value or expression inside the small bracket or parenthesis
