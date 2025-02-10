195b55a89b1de1661411c15a1953f2458018a894
Akbar1001 <149526516+Akbar1001@users.noreply.github.com>
2025-02-10 23:00:36 +0000

    updated

---
+++ 05_iterations/01_for_loop.js
@@ -0,0 +1,17 @@
+//syntax
+for(let i=0;i<10;i++){
+    console.log(i);
+}
+/*
+while(condition)
+{
+    //BODY
+}*/
+
+/*
+let a=[1,3,4,4,5] or s="asdfghjkl"
+for ( const x of arr or s)
+{
+    console.log(x);
+}
+*/
